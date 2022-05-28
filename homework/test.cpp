f4a79211401303053baac5f24f66268c4fb9ae97
RWRW-cpu <1049093323@qq.com>
2022-05-28 23:33:55 +0800

    ac

--- homework/test.cpp
+++ homework/test.cpp
@@ -12,7 +12,8 @@ int *a3[8];
 int *a4[8];
 int *a5[8];
 int *a6[8];
-void init(){
+void init()
+{
     //初始化链表
     a1[0] = &chess[1][3];
     a1[1] = &chess[2][3];
@@ -68,10 +69,10 @@ void init(){
     a6[6] = &chess[6][3];
     a6[7] = &chess[4][1];
 
-    chess[3][3]=-1;
-    chess[4][3]=1;
-    chess[3][4]=1;
-    chess[4][4]=-1;
+    chess[3][3] = -1;
+    chess[4][3] = 1;
+    chess[3][4] = 1;
+    chess[4][4] = -1;
 }
 
 int temp[2];
@@ -99,7 +100,7 @@ bool insearch(int x, int y)
             break;
         }
     }
-    for (int i = 0; i <8; i++)
+    for (int i = 0; i < 8; i++)
     {
         if (&chess[x][y] == a3[i])
         {
@@ -163,7 +164,7 @@ bool insearch(int x, int y)
 //输出棋盘界面
 void gui()
 {
-    system("cls");
+    //system("cls");
     int score_black = 0, score_white = 0;
     printf("\033[0m\n        [黑白棋]\n\n    1 2 3 4 5 6 7 8\n  ┌─────────────────┐\n");
     for (int i = 1; i <= 6; i++)
@@ -198,51 +199,121 @@ void gui()
     // return score_black==score_white ? 0 : ( score_black>score_white ? 1 : -1 ) ;
 }
 
-int main()
-{
-    init();
-    
-    chess[4][1] = -1;
-    chess[4][2] = 1;
-    chess[4][3] = 1;
-    chess[4][4] = -1;
-    chess[4][5] = 1;
-    chess[4][6] = 1;
-    chess[4][7] = 1;
-    chess[4][8] = 1; 
 
-    gui();
-
-
-    //给你一个chess坐标找到他的两条链子
-    if (insearch(4,5))
-    {
-        cout << temp[0] << " " << temp[1] << endl;
-        cout << temp1[0] << " " << temp1[1] << endl;
-    }
-    else {
-        cout<<"error"<<endl;
-    }
-
-
-    
-
-    int row = 0;
-    int color = -1;
+bool react4(int color,int row){
+    bool all=false;
     for (int dx = -1; dx <= 1; dx++)
     { //二向搜索,翻转棋子
         if (dx == 0)
             continue;
-        for (int rowp = row + dx; rowp <= 8 && rowp >= 1; rowp += dx)
+        int times = 1; //记录循环次数
+        int flag = 0;  // 1表示实际 ，0表示虚拟
+        for (int rowp = row + dx; times < 8; rowp += dx, times++)
         {
-            if (*a4[rowp] != -color) break; //不是反色就退出
-            else if (rowp <= 8 && rowp >= 1 && *a4[rowp + dx] == color)
-                for (; rowp != row; rowp -= dx) *a4[rowp] = color; //存在反色棋子则搜寻同色,并翻转中间的所有反色棋子
+            if (rowp >= 8)
+            {
+                rowp -= 8;
+                flag = 1;
+            }
+            if (rowp <= -1)
+            {
+                rowp += 8;
+                flag = -1;
+            }
+            if (flag == 0)
+            {
+                if (*a4[rowp] == color && times == 1)
+                    break; //不是反色就退出
+                else if (*a4[rowp+dx] == color){
+                    for (; rowp != row; rowp -= dx){
+                        *a4[rowp] = color; //存在反色棋子则搜寻同色,并翻转中间的所有反色棋子
+                    }
+                    all=true;
+                    break;
+                    
+                }
+            }
+            else if(flag==-1)
+            {
+                if (*a4[rowp] == color&& times == 1)
+                    break;
+                else if (*a4[rowp ] == color ){
+                    for (; rowp != 8; rowp -= dx)
+                        *a4[rowp] = color; //存在反色棋子则搜寻同色,并翻转中间的所有反色棋子
+                    
+                    rowp -= 8;
+
+                    for(; rowp!=row; rowp-=dx)
+                        *a4[rowp] = color;//存在反色棋子则搜寻同色,并翻转中间的所有反色棋子
+                    all=true;
+                    break;
+                }
+                    
+            }
+            else if(flag==1)
+            {
+                if (*a4[rowp] == color&& times == 1)
+                    break;
+                else if (*a4[rowp ] == color )
+                    {
+                        for (; rowp != -1; rowp -= dx)
+                        *a4[rowp] = color; //存在反色棋子则搜寻同色,并翻转中间的所有反色棋子
+                        
+                        rowp += 8;
+
+                        for(; rowp!=row; rowp-=dx)
+                        *a4[rowp] = color;//存在反色棋子则搜寻同色,并翻转中间的所有反色棋子
+                        all=true;
+                        break;
+                    }
+            }
         }
     }
-    system("pause");
-    gui();
-
-    //判断数组a1两个1之间是否夹着0，如果是，则将a1中的0改为1
+    if(all)
+        return true;
+    else
+        return false;
 }
 
+int main()
+{
+    init();
+
+    chess[4][1] = 1;
+    chess[4][2] = 1;
+    chess[4][3] = 1;
+    chess[4][4] = 1;
+    chess[4][5] = -1;
+    chess[4][6] = 1;
+    chess[4][7] = -1;
+    chess[4][8] = 1;
+
+    gui();
+
+    system("pause");
+    
+    //给你一个chess坐标找到他的两条链子
+    if (insearch(4, 5))
+    {
+        if(temp[0]==4) if(react4(-1,temp[1])) cout<<"yes"<<endl;
+        //cout << temp[0] << " " << temp[1] << endl;
+        cout << temp1[0] << " " << temp1[1] << endl;
+    }
+    else
+    {
+        cout << "error" << endl;
+    }
+
+
+
+
+    int row = 5;
+    int color = -1;
+    
+
+
+
+    
+
+    gui();
+}
